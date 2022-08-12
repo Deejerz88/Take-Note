0b3c6350b0f9929f9c3c65ecb9bfee88bf5b08e6
DJ <djmcmillan93@gmail.com>
2022-08-11 22:36:12 -0400

    removed console.logs, pruned dependencies

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log(`${req.method} request for notes${req.url}`);
   res.sendFile(path.join(__dirname, '../public/notes.html'));
 })
 
